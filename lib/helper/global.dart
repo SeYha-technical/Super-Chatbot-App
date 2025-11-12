5114ebd139bc7fd54e6cb1ac29e0209f5b20eb3c
SeYha-technical <mounseyha0308@gmail.com>
2025-11-12 12:40:24 +0700

    Clear API key in global.dart
    Remove sensitive API key from global.dart

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 const appName = 'Super AI';
 late Size mq;
 //  Google Gemini API Key - https://aistudio.google.com/app/apikey
-String apiKey = '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';
+String apiKey = '';
